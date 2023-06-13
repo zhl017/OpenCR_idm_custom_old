ef85c774a42300bedb94f646dfdd32c8c9f77edf
zhl017 <33046512+zhl017@users.noreply.github.com>
2023-06-13 13:13:04 +0800

    Delete turtlebot3 directory

--- turtlebot3/include/mecanum/mecanum.h
+++
@@ -1,5 +0,0 @@
-#include "turtlebot3_mecanum_motor_driver.h"
-#include "turtlebot3_mecanum_controller.h"
-
-#include "../turtlebot3/turtlebot3_diagnosis.h"
-#include "../turtlebot3/turtlebot3_sensor.h"
--- turtlebot3/src/Mecanum.h
+++
@@ -1,1 +0,0 @@
-#include "../include/mecanum/mecanum.h"
